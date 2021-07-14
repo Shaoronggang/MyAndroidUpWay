d8012bca04895ca6bc089a2a7195fbe1cf62fbd2
shaoronggang <xyronggang@163.com>
2021-07-14 22:19:51 +0800

    提交内容： 1.添加了新的活动

---
+++ app/src/main/java/com/example/myandroidupway/jinjie/activity/RecycleViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.myandroidupway.jinjie.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myandroidupway.R;
+
+public class RecycleViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_view);
+    }
+}
